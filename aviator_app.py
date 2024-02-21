114c89e2312082002982d73fdd6b7c7ce1c218b3
roochieng <49204011+roochieng@users.noreply.github.com>
2024-02-21 20:38:59 +0300

    Aviator and sportpesa updates

--- aviator_app.py
+++ aviator_app.py
@@ -100,6 +100,8 @@ ActionChains(driver).move_to_element(auto_button).perform()
 auto_button.click()
 
 
+time.sleep(1)
+
 # Click auto-cashout to on
 auto_cash_out_switcher = driver.find_element(By.XPATH, '//app-ui-switcher[@class="ng-untouched ng-pristine ng-valid"]/div[@class="input-switch off"]')
 auto_cash_out_switcher.click()
@@ -110,7 +112,7 @@ def second_odd_bet():
     odd_element = driver.find_element(By.XPATH, '//div[@class="cashout-spinner-wrapper"]//input[@class="font-weight-bold"]')
     odd_element.send_keys(Keys.CONTROL + "a")
     odd_element.send_keys(Keys.BACKSPACE)
-    new_text = "1.15"
+    new_text = "1.98"
     odd_element.send_keys(new_text)
 
 
@@ -162,7 +164,7 @@ while status:
     if cleaned_payouts[0:4] != check_list[0:4]:
         previous_cleaned_payouts = cleaned_payouts
         check_list.insert(0, cleaned_payouts[0])
-        if float(check_list[0]) < 1.04 and float(check_list[1]) < 1.04 and float(check_list[2]) < 1.04:
+        if float(check_list[0]) < 1.04 and float(check_list[1]) < 1.04:
             # Update Bet Amount and place bet
             show_notification("Youre pattern is found, bet imediately:", f"Your pattern of: {check_list[0]} and {check_list[1]} ")
             print(f"Round: {nums_of_checks}, odd: {check_list[0]}")
@@ -180,41 +182,41 @@ while status:
             with open(text_file, 'a') as file:
                 file.write(f'{new_data}\n')
 
-        elif float(check_list[0]) > 1.1 and float(check_list[1]) < 1.04 and float(check_list[2]) < 1.04 and float(check_list[3]) < 1.04:
-            show_notification("Youre pattern is found, bet imediately:", f"Your pattern of: {check_list[0]}, {check_list[1]}, {check_list[2]} and {check_list[3]}")
-            # Update Bet Amount and place bet
-            print(f"Round: {nums_of_checks}, odd: {check_list[0]}")
-            first_odd_bet()
-            get_bet_amount()
-            place_bet()
-            print(f"Current Balance: {get_balance()}")
-            print(f"Bet Placed on Pattern 2, stake: {stake(get_balance())}")
-            new_data = {}
-            new_data["odd"] = check_list[0]
-            new_data["datetime"] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-            new_data["round"] = nums_of_checks
-            new_data["odd_bet_placed"] = "Next with Pattern 1"
-            dict_list.append(new_data)
-            with open(text_file, 'a') as file:
-                file.write(f'{new_data}\n')
+        # elif float(check_list[1]) < 1.04 and float(check_list[2]) < 1.04 and float(check_list[3]) < 1.04:
+        #     show_notification("Youre pattern is found, bet imediately:", f"Your pattern of: {check_list[0]}, {check_list[1]}, {check_list[2]} and {check_list[3]}")
+        #     # Update Bet Amount and place bet
+        #     print(f"Round: {nums_of_checks}, odd: {check_list[0]}")
+        #     first_odd_bet()
+        #     get_bet_amount()
+        #     place_bet()
+        #     print(f"Current Balance: {get_balance()}")
+        #     print(f"Bet Placed on Pattern 2, stake: {stake(get_balance())}")
+        #     new_data = {}
+        #     new_data["odd"] = check_list[0]
+        #     new_data["datetime"] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        #     new_data["round"] = nums_of_checks
+        #     new_data["odd_bet_placed"] = "Next with Pattern 1"
+        #     dict_list.append(new_data)
+        #     with open(text_file, 'a') as file:
+        #         file.write(f'{new_data}\n')
 
-        elif float(check_list[0]) > 1.1 and float(check_list[1]) > 1.1 and float(check_list[2]) < 1.04 and float(check_list[3]) < 1.04 and float(check_list[4]) < 1.04:
-            show_notification("Youre pattern is found, bet imediately:", f"Your pattern of: {check_list[0]}, {check_list[1]}, {check_list[2]}, {check_list[3]} and {check_list[4]}")
-            # Update Bet Amount and place bet
-            print(f"Round: {nums_of_checks}, odd: {check_list[0]}")
-            first_odd_bet()
-            get_bet_amount()
-            place_bet()
-            print(f"Current Balance: {get_balance()}")
-            print(f"Bet Placed on Pattern 2, stake: {stake(get_balance())}")
-            new_data = {}
-            new_data["odd"] = check_list[0]
-            new_data["datetime"] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-            new_data["round"] = nums_of_checks
-            new_data["odd_bet_placed"] = "Next with Pattern 1"
-            dict_list.append(new_data)
-            with open(text_file, 'a') as file:
-                file.write(f'{new_data}\n')
+        # elif float(check_list[2]) < 1.04 and float(check_list[3]) < 1.04 and float(check_list[4]) < 1.04:
+        #     show_notification("Youre pattern is found, bet imediately:", f"Your pattern of: {check_list[0]}, {check_list[1]}, {check_list[2]}, {check_list[3]} and {check_list[4]}")
+        #     # Update Bet Amount and place bet
+        #     print(f"Round: {nums_of_checks}, odd: {check_list[0]}")
+        #     first_odd_bet()
+        #     get_bet_amount()
+        #     place_bet()
+        #     print(f"Current Balance: {get_balance()}")
+        #     print(f"Bet Placed on Pattern 2, stake: {stake(get_balance())}")
+        #     new_data = {}
+        #     new_data["odd"] = check_list[0]
+        #     new_data["datetime"] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        #     new_data["round"] = nums_of_checks
+        #     new_data["odd_bet_placed"] = "Next with Pattern 1"
+        #     dict_list.append(new_data)
+        #     with open(text_file, 'a') as file:
+        #         file.write(f'{new_data}\n')
 
         else:
             new_data = {}
